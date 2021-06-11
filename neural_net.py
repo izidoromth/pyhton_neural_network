fea5a17dbabb7c2016eb3a528f46595a976a486c
Matheus Izidoro de Almeida <izidoro@radek.com.br>
2021-06-11 09:22:49 -0300

    creates three neuron input layer

--- neural_net.py
+++ neural_net.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 input = [.7, .9, .5, .4]
-weight = [.2, .3, .4, .2]
+weights = [[.2, .3, .4, .2],
+           [.6, .2, .9, .4],
+           [.6, .3, .7, .5]]
+bias = [.6, .8, .4]
 
-print(np.dot(input, weight))
+print(np.dot(weights, input) + bias)
