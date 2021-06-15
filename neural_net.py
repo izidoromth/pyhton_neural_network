d37edba21947e2c52535577dbea50a8dc16c1c22
Matheus Izidoro de Almeida <izidoro@radek.com.br>
2021-06-15 13:52:47 -0300

    add layer_dense and activation_relu

--- neural_net.py
+++ neural_net.py
@@ -1,9 +1,25 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral, spiral_data
 
-input = [.7, .9, .5, .4]
-weights = [[.2, .3, .4, .2],
-           [.6, .2, .9, .4],
-           [.6, .3, .7, .5]]
-bias = [.6, .8, .4]
+nnfs.init()
 
-print(np.dot(weights, input) + bias)
+X, y = spiral_data(100,3)
+
+class Layer_Dense():
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10*np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases 
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
+
+layer1.forward(X)
+activation1.forward(layer1.output)
+print(activation1.output)
